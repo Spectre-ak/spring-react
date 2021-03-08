13d3e5c81ddeb96bfcce98194d0ba22fe54d1eb9
Akash Upadhyay <62694340+Spectre-ak@users.noreply.github.com>
2021-03-08 16:01:12 +0530

    Add files via upload

---
+++ react/src/App.js
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import './App.css';
+import ReactDOM from 'react-dom';
+alert("sss")
+ReactDOM.render(<h1>hdd</h1>,document.getElementById("root"))
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React asasasda sssssssss
+        </a> 
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+
+function tryIt(argument) {
+  // body...
+alert("hello worlf")
+
+}
